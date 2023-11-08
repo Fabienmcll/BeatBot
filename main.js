29fea1751d2157cb64cb7032faac99de830fbdfb
Fabienmcl <fabienmclll@gmail.com>
2023-11-08 11:27:13 +0100

    Handler des commandes, command ping

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField, Collection } = require("discord.js");
 const client = new Client({intents: new IntentsBitField(3276799)});
+const loadCommands = require("./loaders/loeadCommands");
 const dotenv = require("dotenv");
 dotenv.config();
 
+client.commands = new Collection();
+
+loadCommands(client);
+
+client.on("messageCreate", message =>{
+
+    const prefix = "!";
+    if(!message.content.startsWith(prefix)) return;
+
+    const name = message.content.split(" ")[0].replace(prefix, "");
+    const command = client.commands.get(name);
+
+    command.run(client, message);
+} )
+
 client.login(process.env.TOKEN);
+
